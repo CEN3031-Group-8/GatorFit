0e6beab26a9a4dd33bb6ee3401e33119e564777d
Will Pearce <pearcewilliam222@gmail.com>
2024-10-12 01:23:19 -0400

    updated backend default port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 // Entry point for the app
 import app from './index'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`GatorFit Backend Up\nlistening on port ${port}`)
 })
