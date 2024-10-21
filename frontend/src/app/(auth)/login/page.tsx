7e05e892dc14e0052cf18242a5a85a246d756e60
Will Pearce <pearcewilliam222@gmail.com>
2024-10-21 15:34:51 -0400

    refactored login page

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -8,31 +8,29 @@ export const metadata = {
   description: 'Login to access GatorFit',
 }
 
-export default function LoginPage() {
-  return (
-    <>
-      <h1 className='mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white'>
-        Sign In
-      </h1>
+const LoginPage = () => (
+  <>
+    <h1 className='mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white'>
+      Sign In
+    </h1>
 
-      <LoginForm />
+    <LoginForm />
 
+    <a
+      href='#'
+      className='flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600'
+    >
+      <FcGoogle className='w-6 h-6 mx-2' />
+      <span className='mx-2'>Continue with Google</span>
+    </a>
+
+    <div className='mt-6 text-center '>
       <a
         href='#'
-        className='flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600'
+        className='text-sm text-blue-500 hover:underline dark:text-blue-400'
       >
-        <FcGoogle className='w-6 h-6 mx-2' />
-        <span className='mx-2'>Continue with Google</span>
+        Don’t have an account yet? Sign up
       </a>
-
-      <div className='mt-6 text-center '>
-        <a
-          href='#'
-          className='text-sm text-blue-500 hover:underline dark:text-blue-400'
-        >
-          Don’t have an account yet? Sign up
-        </a>
-      </div>
-    </>
-  )
-}
+    </div>
+  </>
+)
