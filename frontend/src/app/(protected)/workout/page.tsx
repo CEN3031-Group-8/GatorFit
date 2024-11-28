a27414bd2d16c45f8615b2668789c51d009d684e
CyrusKell <cyfeye@gmail.com>
2024-11-27 22:17:46 -0500

    created workout page and made the default page redirect to /workout

---
+++ frontend/src/app/(protected)/workout/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Workout - GatorFit',
+  description: 'Log your workout!',
+}
+
+const WorkoutPage = () => (
+  <>
+    <h1 className='mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white'>
+      Workout
+    </h1>
+  </>
+)
+
+export default WorkoutPage
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -2,6 +2,8 @@ export const publicRoutes = ['/']
 
 export const authRoutes = ['/login', '/register']
 
+export const protectedRoutes = ['/settings', '/workout']
+
 export const apiAuthPrefix = '/api/auth'
 
 export const DEFAULT_LOGIN_REDIRECT = '/settings'
